5ff82ee1f6f3640aff0f6e24eda1fa00b832a11f
Kanumone <KuzminAAwork@yandex.ru>
2023-09-09 16:23:06 +0700

    fix some bugs

--- app.js
+++ app.js
@@ -1,18 +1,28 @@
 const buttons = document.querySelectorAll(".key");
 const display = document.querySelector(".display");
 
-let firstNumber = (secondNumber = operator = "");
+let firstNumber, secondNumber, operator;
+
+function clear() {
+  firstNumber = secondNumber = operator = "";
+  display.textContent = "0";
+};
+clear();
 
 buttons.forEach((button) => {
-  button.addEventListener("mousedown", () => {
+  button.addEventListener("mousedown", (event) => {
+    const button = event.target;
     button.style.cssText = "opacity: 50%; transform: scale(1.05);";
   });
 
-  button.addEventListener("mouseup", () => {
+  button.addEventListener("mouseup", (event) => {
+    const button = event.target;
     button.style.cssText = "opacity: 100%; transform: scale(1);";
   });
 
-  button.addEventListener("click", () => {
+  button.addEventListener("click", (event) => {
+    const button = event.target;
+
     mainProcess(button);
   });
 });
@@ -20,7 +30,6 @@ buttons.forEach((button) => {
 function mainProcess(button) {
   const input = checkButton(button);
   const value = button.textContent;
-
   if (input === "number" && !operator) {
     firstNumber += value;
     display.textContent = firstNumber;
@@ -28,8 +37,7 @@ function mainProcess(button) {
     secondNumber += value;
     display.textContent = secondNumber;
   } else if (input === "AC") {
-    firstNumber = secondNumber = operator = "";
-    display.textContent = "0";
+    clear();
   } else if (input === "operator") {
     operator = value;
   } else {
@@ -56,8 +64,8 @@ function operate() {
 }
 
 function checkButton(button) {
-  const operators = /\+|-|\*|\/|\^/;
-  const numbers = /\d/;
+  const operators = new RegExp("[+*/-]");
+  const numbers = new RegExp("\\d");
   const value = button.textContent;
   let result;
   if (operators.test(value)) {
